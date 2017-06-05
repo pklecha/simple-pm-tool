160cb96499d9f28b2b337be8cf3fed708da0a85e
pawel.klecha@gmail.com <tokarka1>
2017-06-06 00:41:11 +0200

    html templates FOS User Bundle - working login

--- src/PMToolBundle/Controller/DefaultController.php
+++ src/PMToolBundle/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('PMToolBundle:Default:index.html.twig');
+        return $this->render(':default:projects.html.twig');
     }
 }
