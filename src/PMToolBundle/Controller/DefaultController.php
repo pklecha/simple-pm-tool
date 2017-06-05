f1c362f3d14a08f47350d64aa3aca3f454335297
pawel.klecha@gmail.com <tokarka1>
2017-06-06 01:18:21 +0200

    basic app structure (actions + static templates)

--- src/PMToolBundle/Controller/DefaultController.php
+++ src/PMToolBundle/Controller/DefaultController.php
@@ -8,10 +8,34 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="dashboard")
      */
     public function indexAction()
     {
+        return $this->render(':default:dashboard.html.twig');
+    }
+
+    /**
+     * @Route("/projects", name="projects")
+     */
+    public function projectsAction()
+    {
         return $this->render(':default:projects.html.twig');
+    }
+
+    /**
+     * @Route("/people", name="people")
+     */
+    public function peopleAction()
+    {
+        return $this->render(':default:people.html.twig');
+    }
+
+    /**
+     * @Route("/files", name="files")
+     */
+    public function filesAction()
+    {
+        return $this->render(':default:files.html.twig');
     }
 }
